1eccad3fbe47d9f58d08f9db284faaa8d9e49b15
José Eduardo Ormeño Meneses <jormenom@odybank.com.pe>
2023-11-10 19:15:43 -0500

    fix: update schedule

--- pages/schedule.tsx
+++ pages/schedule.tsx
@@ -3,7 +3,7 @@ import Header from './../components/header.component';
 import styles from '@/styles/pages/schedule.module.scss';
 import HeadPage from '@/components/head.component';
 import { useEffect, useState } from 'react';
-import { TrackOne, TrackTwo, TrackThree } from '@/data/dataTrack';
+import { TrackOne, TrackTwo, TrackThree, TrackFour, TrackFive } from '@/data/dataTrack';
 
 
 export default function Schedule() {
@@ -18,9 +18,11 @@ export default function Schedule() {
             <main className={styles.container}>
                 <h1 className={styles.title}>Agenda</h1>
                 <p className={styles.tabs}>
-                    <span className={`${styles.tab} ${indexTrack === 1 ? styles.active : ''}`} onClick={() => setIndexTrack(1)}>Tack 1</span>
-                    <span className={`${styles.tab} ${indexTrack === 2 ? styles.active : ''}`} onClick={() => setIndexTrack(2)}>Tack 2</span>
-                    <span className={`${styles.tab} ${indexTrack === 3 ? styles.active : ''}`} onClick={() => setIndexTrack(3)}>Tack 3</span>
+                    <span className={`${styles.tab} ${indexTrack === 1 ? styles.active : ''}`} onClick={() => setIndexTrack(1)}>Track 1</span>
+                    <span className={`${styles.tab} ${indexTrack === 2 ? styles.active : ''}`} onClick={() => setIndexTrack(2)}>Track 2</span>
+                    <span className={`${styles.tab} ${indexTrack === 3 ? styles.active : ''}`} onClick={() => setIndexTrack(3)}>Track 3</span>
+                    <span className={`${styles.tab} ${indexTrack === 4 ? styles.active : ''}`} onClick={() => setIndexTrack(4)}>Track 4</span>
+                    <span className={`${styles.tab} ${indexTrack === 5 ? styles.active : ''}`} onClick={() => setIndexTrack(5)}>Track 5</span>
                 </p>
                 {
                     indexTrack === 1 && (
@@ -67,6 +69,48 @@ export default function Schedule() {
                 {
                     indexTrack === 3 && (
                         TrackThree && TrackThree.map(slot =>
+                            <div className={styles.cardEvent} key={slot.theme}>
+                                <p className={styles.title}>{slot.theme}</p>
+                                <small className={styles.timetable}>{slot.start} - {slot.end}</small>
+                                {
+                                    slot.photo && (
+                                        <div className={styles.speaker}>
+                                            <img className={styles.photo} src={`/./assets/images/speakers/${slot.photo}`} alt={slot.speaker} />
+                                            <p className={styles.name}>{slot.speaker}</p>
+                                        </div>
+                                    )
+                                }
+                                <p className={styles.talkDescription}>
+                                    {slot.description}
+                                </p>
+                            </div >
+                        )
+                    )
+                }
+                {
+                    indexTrack === 4 && (
+                        TrackFour && TrackFour.map(slot =>
+                            <div className={styles.cardEvent} key={slot.theme}>
+                                <p className={styles.title}>{slot.theme}</p>
+                                <small className={styles.timetable}>{slot.start} - {slot.end}</small>
+                                {
+                                    slot.photo && (
+                                        <div className={styles.speaker}>
+                                            <img className={styles.photo} src={`/./assets/images/speakers/${slot.photo}`} alt={slot.speaker} />
+                                            <p className={styles.name}>{slot.speaker}</p>
+                                        </div>
+                                    )
+                                }
+                                <p className={styles.talkDescription}>
+                                    {slot.description}
+                                </p>
+                            </div >
+                        )
+                    )
+                }
+                {
+                    indexTrack === 5 && (
+                        TrackFive && TrackFive.map(slot =>
                             <div className={styles.cardEvent} key={slot.theme}>
                                 <p className={styles.title}>{slot.theme}</p>
                                 <small className={styles.timetable}>{slot.start} - {slot.end}</small>
